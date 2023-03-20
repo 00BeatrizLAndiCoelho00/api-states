0891bb32a91004d912c81aa7b6eb4b4b79be24f1
ANDRESSA BEATRIZ FIDELIS LANDI COELHO <22282076@CFP127ALUNO.LOCAL>
2023-03-20 11:01:59 -0300

    avanço?

--- app.js
+++ app.js
@@ -19,12 +19,24 @@ mapa.addEventListener('click', getEstado)
 //this another boy create the box where the information goes on
 //a very generic div 
 const infoMap = document.getElementById("info-map");
-
 const box = document.createElement("div");
-
-let boxStyle = '<div class="boxInfos"></div>'
-box.innerHTML = boxStyle;
-
+// let a = "zzzz"
+// box.textContent = a
 box.classList.add("box");
-
 infoMap.appendChild(box);
+
+//________________________________this boy, the smal and adorable circle___________________
+const circleMap = document.getElementById("circleMap");
+const circle = document.createElement("div");
+
+let contentNameStatde = "nome estado"
+let contentTextCapital = "capital"
+let contentTextRegiao = "regiao"
+
+box.textContent = contentTextCapital + contentTextCapital + contentTextRegiao
+box.style.
+box.classList.add("box");
+infoMap.appendChild(circle);
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+//this baby rigth there makes the api works 
